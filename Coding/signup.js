c26637c5179d57ef9abaa596b599f2cf33af32c5
Fireflame14 <145296407+Fireflame14@users.noreply.github.com>
2023-09-17 16:28:00 -0500

    Add files via upload

---
+++ Coding/signup.js
@@ -0,0 +1,13 @@
+function signup(){
+var a,b;
+a=document.getElementById("firstname").value;
+if(a=null || a=" "){
+b="Please Enter First Name";
+}
+document.getElementById("msg1").innerHTML=b;
+
+
+alert("Please Enter Name");
+
+
+}
